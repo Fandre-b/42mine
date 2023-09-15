e62d5bbdc8c4b2e9bb19c20e7e57ddd5e46c51a7
Juka Redmi <filipe.antunes94@gmail.com>
2023-09-15 16:59:09 +0100

    a

---
+++ Trabalhos/libft/libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fandre-b <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 12:19:11 by fandre-b          #+#    #+#             */
+/*   Updated: 2023/09/06 15:59:13 by fandre-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stflib.h>
+# include <unistd.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+int	ft_islower(int c);
+int	ft_isupper(int c);
+int	ft_isblank(int c);
+int	ft_isspace(int c);
+
+#endif
