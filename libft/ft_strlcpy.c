51c5d56f863ff450766105f075449c2537c1be79
JukaPc <filipeantunes94@gmail.com>
2023-09-25 22:07:10 +0100

    amotemuitobaby

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fandre-b <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 15:33:32 by fandre-b          #+#    #+#             */
+/*   Updated: 2023/09/21 00:37:00 by fandre-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t  ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	src_len;
+
+	src_len = ft_strlen(src);
+
+	if (size == 0)
+		return (src_len);
+	i = 0;
+	while(src[i] && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (src_len);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -21,21 +21,16 @@ int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
-int	ft_islower(int c);
-int	ft_isupper(int c);
 int	ft_isblank(int c);
 int	ft_isspace(int c);
 int	ft_isalnum(int c);
-size_t	ft_strlen(const char *str);
-char	*ft_strcat(char *dst, const char *src);
-char	*ft_strchr(char *str, int c);
-char	*ft_strcpy(char *dst, const char *src);
-//size_t	ft_strlcat(char *dst, const char *src, size_t size);
-char	*ft_strncat(char *dst, const char *src, size_t size);
-char	*ft_strncpy(char *dst, const char *src, size_t size);
-char	*ft_strnstr(const char *str, const char *to_find, size_t size);
-char	*ft_strrchr(const char *str, int c);
 int	ft_tolower(int c);
 int	ft_toupper(int c);
+size_t	ft_strlen(const char *str);
+size_t  ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strchr(char *str, int c);
+char	*ft_strrchr(const char *str, int c);
+char	*ft_strnstr(const char *str, const char *to_find, size_t size);
 
 #endif
