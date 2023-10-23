2df546b8343517bf1ccec19b0ee0e235e5d8f00c
Sofia Angelico <sofiaangelico@gmail.com>
2023-10-23 22:57:00 +0100

    a

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -14,18 +14,49 @@
 
 t_list  *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    t_list  *newlst;
-    t_list  *newelem;
+    t_list  *new_lst;
+    t_list  *new_elem;
 
     while(lst)
     {
-        newelem = ft_lstnew(lst->content);
-        if (newelem)
-            ft_lstadd_back(&newlst, newelem);
+        new_elem = ft_lstnew(lst->content);
+        ft_lstiter(new_elem, void (*f)(void *))
+        if (new_elem)
+            ft_lstadd_back(&new_lst, new_elem);
         else
-            ft_lstdelone(newelem, del);
+            ft_lstdelone(new_elem, del);
             return (NULL);
         lst = lst->next;
     }
-    return (newlst)
+    return (new_lst)
+}
+
+int main(void)
+{
+    t_list *lst;
+    t_list *new_lst ,
+
+    lst = ft_lstnew(ft_strdup("1"));
+    ft_lstadd_back(&lst, ft_lstnew(ft_strdup("2")));
+    ft_lstadd_back(&lst, ft_lstnew(ft_strdup("3")));
+    ft_lstmap(lst, add_one, del);
+
+    printf("Original List:\n");
+    while (lst)
+    {
+        printf("%s\n", (char *)(lst->content));
+        lst = lst->next;
+    }
+
+    printf("\nModified List (added 1 to each element):\n");
+    while (new_lst)
+    {
+        printf("%d\n", *((int *)(new_lst->content)));
+        new_lst = new_lst->next;
+    }
+
+    ft_lstclear(&lst, del);
+    ft_lstclear(&new_lst, del);
+
+    return (0);
 }
